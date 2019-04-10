53894dbb8b8b5274f87d8ea694b320d6eba452a8
GianFire <gian.delprado94@gmail.com>
2019-04-10 11:38:32 -0700

    finsihed project, all test pass, server deployed

--- server/app.js
+++ server/app.js
@@ -1,10 +1,19 @@
 // import files and packages up here
-
+var data = require('./data.json');
+var morgan = require('morgan');
 
 // create your express server below
-var app;
+var express = require('express');
+var app = express();
 
 // add your routes and middleware below
+app.use(express.static('../startnow-web101-san-diego-top-spots/'));
+app.get('/', function(req, res) {
+    res.status(200).send('oh, its working...');
+});
 
+app.get('/data', function(req, res) {
+    res.status(200).send(data);
+});
 // finally export the express application
 module.exports = app;
